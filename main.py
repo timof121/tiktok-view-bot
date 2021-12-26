3ff7f7e066e56cf0681359d5d3d180cc276d3543
timof121 <52547763+timof121@users.noreply.github.com>
2021-12-26 12:15:48 +0100

    better

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 import requests as r
 import random
+from time import sleep
 
 headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 5.2; Win64; x64; rv:60.9) Gecko/20100101 Goanna/4.3 Firefox/60.9 Mypal/28.6.1',
+    'authority': 'www.tiktok.com',
+    'origin': 'https://www.tiktok.com',
 }
 
 proxy = set()
@@ -19,4 +22,8 @@ e = input('Video link: ')
 
 while True:
     output = r.get(e, proxies=proxies)
-    print(output)
+    if output.ok:
+        print('[+]Request succesfully sent!')
+    else:
+        print('[-]Failed to send request!')
+    sleep(2)
